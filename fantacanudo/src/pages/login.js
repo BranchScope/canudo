b724c025a85a728aef0b04382d7595633b2d9d81
BranchScope <rocco4064@gmail.com>
2023-08-05 03:27:29 +0200

    initializing iasteme for fantacanudo frontend

--- fantacanudo/src/pages/login.js
+++ fantacanudo/src/pages/login.js
@@ -13,41 +13,44 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <form className="bg-gray-950 shadow-md rounded px-10 pt-8 pb-10 mb-4" onSubmit={handleLogin}>
-        <h2 className="text-2xl font-bold mb-4 text-white justify-center">FantaCanudo</h2>
+    <div className="bg-gray-950 flex items-center justify-center h-screen">
+      <form className="bg-slate-900 shadow-md rounded-[18px] px-16 pt-10 pb-10 mb-10" onSubmit={handleLogin}>
+        <h2 className="text-3xl font-bold mb-1 text-white justify-center">FantaCanudo</h2>
+        <h2 className="text-md font-italic mb-4 text-gray-400 justify-center">Il primo FantaScuola digitale in italia!</h2>
         <div className="mb-4">
-          <label htmlFor="username" className="block text-white-950 text-sm font-bold mb-2">Username:</label>
+          <label htmlFor="username" className="block text-white-950 text-md font-bold mb-2">Username:</label>
           <input
             type="text"
             id="username"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border border-gray-500 focus:border-white rounded-lg w-full py-2 px-3 bg-slate-900 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div className="mb-6">
-          <label htmlFor="password" className="block text-white-950 text-sm font-bold mb-2">Password:</label>
+          <label htmlFor="password" className="block text-white-950 text-md font-bold mb-2">Password:</label>
           <input
             type="password"
             id="password"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border border-gray-500 focus:border-white rounded-lg w-full py-2 px-3 bg-slate-900 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button
-          type="submit"
-          className="bg-blue-950 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Accedi
-        </button>
-        <button
-          type="submit"
-          className="bg-blue-950 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Registrati
-        </button>
+        <div className="flex items-center justify-center space-x-4">
+          <button
+            type="submit"
+            className="bg-blue-950 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
+          >
+            Accedi
+          </button>
+          <button
+            type="submit"
+            className="bg-blue-950 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline"
+          >
+            Registrati
+          </button>
+        </div>
       </form>
     </div>
   );
