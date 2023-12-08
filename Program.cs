d3e3e4d8b895211cc6574f88544f6ac644e66239
Rocco Spadone <rocco4064@gmail.com>
2023-12-08 19:15:31 +0100

    method validator

--- Program.cs
+++ Program.cs
@@ -2,19 +2,25 @@
 using RestSharp;
 
 namespace canudo_news;
-
 internal static class Umbrella
 {
     private static readonly string? Token = Environment.GetEnvironmentVariable("TOKEN");
 
     private const string Resource = "https://api.telegram.org/bot";
 
+    private static readonly List<string?> AllowedMethods = new List<string?>() { "getMe", "sendMessage" };
+
     private static async Task Main()
     {
         Console.Write("Enter the method: ");
         var method = Console.ReadLine();
 
-        var client = new RestClient((string)(Resource + Token));
+        if (!AllowedMethods.Contains(method))
+        {
+            throw new MissingMethodException("This method has not been implemented yet.");
+        }
+        
+        var client = new RestClient(Resource + Token);
         var request = new RestRequest(method, Method.Post);
         var response = await client.GetAsync(request);
 
