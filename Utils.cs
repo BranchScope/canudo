2a90bcffedd1f2dc15d5b12e15ca371df2a5d27c
Rocco Spadone <rocco4064@gmail.com>
2023-12-09 16:49:54 +0100

    iterate a foreach with await (honorable mention for this one)

--- Utils.cs
+++ Utils.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Playwright;
+﻿using System.Collections;
+using Microsoft.Playwright;
 
 namespace canudo_news;
 
@@ -13,7 +14,12 @@ public static class Scraper
         var page = await browser.NewPageAsync();
         await page.GotoAsync(Resource);
         var firstJobElement = page.Locator("div[class='items-leading clearfix']").Locator("a[href]").First;
+        var othersJobsElements = page.Locator("div[class='item column-1 span12']").Locator("a[href][itemprop='url']");
         Console.WriteLine(Resource + await firstJobElement.GetAttributeAsync("href"));
+        foreach (var element in await othersJobsElements.ElementHandlesAsync())
+        {
+            Console.WriteLine(Resource + await element.GetAttributeAsync("href"));
+        }
         return null;
     }
 }
