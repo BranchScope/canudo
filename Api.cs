dbe76f096075bd109cbad1aea1bd01b7b042d5be
Rocco Spadone <rocco4064@gmail.com>
2023-12-09 02:59:58 +0100

    refactor + botapi class + tests

---
+++ Api.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using RestSharp;
+
+namespace canudo_news;
+
+public static class BotApi
+{
+    private const string Resource = "https://api.telegram.org/bot";
+    private static readonly string? Token = Environment.GetEnvironmentVariable("TOKEN");
+
+    private static async Task<dynamic?> GetMe()
+    {
+        var client = new RestClient(Resource + Token);
+        var request = new RestRequest("getMe", Method.Post);
+        var response = await client.GetAsync(request);
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            return response.Content;
+        }
+        else
+        {
+            Console.WriteLine("Oh, it seems like something did go wrong, pls check how many pilngul you have before debugging and then defenestrate yourself from the attic of Burshcsdh-Kalifa.");
+            return false;
+        }
+    }
+
+    private static async Task<dynamic?> SendMessage(int chatId, string text)
+    {
+        var client = new RestClient(Resource + Token);
+        var request = new RestRequest("sendMessage", Method.Post);
+        var param = new { chat_id = chatId, text = text };
+        Console.WriteLine(ObjectDumper.Dump(param));
+        request.AddJsonBody(param);
+        var response = await client.GetAsync(request);
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            return response.Content;
+        }
+        else
+        {
+            Console.WriteLine("Oh, it seems like something did go wrong, pls check how many pilngul you have before debugging and then defenestrate yourself from the attic of Burshcsdh-Kalifa.");
+            return false;
+        }
+    }
+
+    public static async Task Test()
+    {
+        var test = await SendMessage(1876496621, "Sent with C#, but can't see more sharp as said in the docs.");
+        Console.WriteLine($"The output: {test}");
+    }
+    
+}
---
+++ OldTest.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+
+namespace canudo_news;
+internal static class Umbrella
+{
+    private static readonly string? Token = Environment.GetEnvironmentVariable("TOKEN");
+
+    private const string Resource = "https://api.telegram.org/bot";
+
+    private static readonly List<string?> AllowedMethods = new List<string?>() { "getMe", "sendMessage" };
+
+    private static async Task MainZ()
+    {
+        Console.Write("Enter the method: ");
+        var method = Console.ReadLine();
+
+        if (!AllowedMethods.Contains(method))
+        {
+            throw new MissingMethodException("This method has not been implemented yet.");
+        }
+        
+        var client = new RestClient(Resource + Token);
+        var request = new RestRequest(method, Method.Post);
+        var response = await client.GetAsync(request);
+
+        Console.WriteLine(response.Content);
+    }
+}
