85ad2052c3b1dd7c67997353b03d826772f4aeae
Rocco Spadone <rocco4064@gmail.com>
2024-06-22 22:33:07 +0200

    added dbuser class + db functions

---
+++ canudo_libri/Database/DBType.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace canudo_libri.Database;
+
+public class DBUser : IEnumerable
+{
+    public long UserId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Lang { get; set; }
+    public string Status { get; set; }
+    public int Rank { get; set; }
+    public bool Banned { get; set; }
+    public IEnumerator GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
--- canudo_libri/Database/Database.cs
+++ canudo_libri/Database/Database.cs
@@ -1,4 +1,6 @@
-﻿using Npgsql;
+﻿using System.Runtime.InteropServices.JavaScript;
+using Npgsql;
+using NpgsqlTypes;
 
 namespace canudo_libri.Database;
 
@@ -22,7 +24,7 @@ public class Database
 
     public static async Task<long> CheckUser(NpgsqlConnection db, long userId)
     {
-        var query = "SELECT COUNT(*) FROM users WHERE user_id = @user_id";
+        const string query = "SELECT COUNT(*) FROM users WHERE user_id = @user_id";
         var cmd = new NpgsqlCommand(query, db);
         cmd.Parameters.AddWithValue("user_id", userId);
         await cmd.PrepareAsync();
@@ -32,17 +34,78 @@ public class Database
 
     public static async Task<int> AddUser(NpgsqlConnection db, User user)
     {
-        var query =
-            "INSERT INTO users(user_id, first_name, last_name, username, lang) VALUES(@user_id, @first_name, @last_name, @username, @lang) ON CONFLICT (user_id) DO UPDATE SET first_name = @first_name, last_name = @last_name, username = @username";
+        const string query = "INSERT INTO users(user_id, first_name, last_name, username, lang, status, rank, banned) VALUES(@user_id, @first_name, @last_name, @username, @lang, @status, @rank, @banned) ON CONFLICT (user_id) DO UPDATE SET first_name = @first_name, last_name = @last_name, username = @username";
         var cmd = new NpgsqlCommand(query, db);
         cmd.Parameters.AddWithValue("user_id", user.Id);
         cmd.Parameters.AddWithValue("first_name", user.FirstName);
         cmd.Parameters.AddWithValue("last_name", user.LastName ?? "");
         cmd.Parameters.AddWithValue("username", user.Username ?? "");
         cmd.Parameters.AddWithValue("lang", user.LanguageCode ?? "");
+        cmd.Parameters.AddWithValue("status", "");
+        cmd.Parameters.AddWithValue("rank", "1");
+        cmd.Parameters.AddWithValue("banned", false);
         await cmd.PrepareAsync();
         var r = await cmd.ExecuteNonQueryAsync();
         return r;
+    }
+
+    public static async Task<int> GetSubs(NpgsqlConnection db)
+    {
+        const string query = "SELECT COUNT(*) FROM users";
+        var cmd = new NpgsqlCommand(query, db);
+        var count = (int)await cmd.ExecuteScalarAsync();
+        return count;
+    }
+
+    public static async Task<List<DBUser>> GetUsers(NpgsqlConnection db)
+    {
+        const string query = "SELECT * FROM users";
+        var cmd = new NpgsqlCommand(query, db);
+        var reader = await cmd.ExecuteReaderAsync();
+        List<DBUser> results = [];
+        while (await reader.ReadAsync())
+        {
+            var element = new DBUser()
+            {
+                FirstName = reader.GetString(0),
+                LastName = reader.GetString(1),
+                Username = reader.GetString(2),
+                Lang = reader.GetString(3),
+                Status = reader.GetString(4),
+                Rank = reader.GetInt32(5),
+                Banned = reader.GetBoolean(6),
+            };
+            results.Add(element);
+        }
+
+        return results;
+    }
+
+    public static async Task<DBUser> GetUser(NpgsqlConnection db, long userId)
+    {
+        const string query = "SELECT * FROM users WHERE user_id = @user_id";
+        var cmd = new NpgsqlCommand(query, db);
+        cmd.Parameters.AddWithValue("user_id", userId);
+        await cmd.PrepareAsync();
+        var dr = await cmd.ExecuteReaderAsync();
+        
+        if (dr.Read() == false)
+        {
+            await dr.CloseAsync();
+            return [];
+        }
+
+        var user = new DBUser() { 
+            FirstName = (string)dr[0], 
+            LastName = (string)dr[1]),
+            Username = (string)dr[2],
+            Lang = (string)dr[3],
+            Status = (string)dr[4],
+            Rank = (int)dr[5],
+            Banned = (bool)dr[6]
+        };
+        await dr.CloseAsync();
+        return user;
     }
 
 }
